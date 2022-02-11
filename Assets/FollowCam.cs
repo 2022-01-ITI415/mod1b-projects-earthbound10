e70159ffd8294936f7713ec15031d7db72d57c51
earthbound10 <themikechang@yahoo.com>
2022-02-10 20:44:17 -0500

    Castle Built

--- Assets/FollowCam.cs
+++ Assets/FollowCam.cs
@@ -8,6 +8,7 @@ public class FollowCam : MonoBehaviour
     [Header("Set Dynamically")]
     [Header("Set in Inspector")]
     public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
     public float camZ;
     void Awake(){
         camZ = this.transform.position.z;
@@ -15,9 +16,12 @@ public class FollowCam : MonoBehaviour
     void FixedUpdate(){
         if(POI == null) return;
         Vector3 destination = POI.transform.position;
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
         transform.position = destination;
+        Camera.main.orthographicSize = destination.y + 10;
     }
     void Start()
     {
